955692db10cfe9bf8ac407c3718377d088c6d043
hoyoung <hoyoung@sina.cn>
2015-11-15 23:52:07 +0800

    f

--- sql/haodf.sql
+++ sql/haodf.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50626
 File Encoding         : 65001
 
-Date: 2015-11-12 23:38:07
+Date: 2015-11-15 23:51:56
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -77,7 +77,38 @@ CREATE TABLE `disease_cat` (
 -- ----------------------------
 DROP TABLE IF EXISTS `doctor`;
 CREATE TABLE `doctor` (
-  `doc_id` varchar(36) NOT NULL,
+  `doc_id` varchar(45) NOT NULL,
+  `doc_name` varchar(45) DEFAULT NULL,
+  `home_url` varchar(255) DEFAULT NULL,
+  `info_url` varchar(255) DEFAULT NULL,
+  `status` int(1) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `hos_id` varchar(255) DEFAULT NULL,
+  `hos_name` varchar(255) DEFAULT NULL,
+  `hos_url` varchar(255) DEFAULT NULL,
+  `dept_id` varchar(255) DEFAULT NULL,
+  `dept_name` varchar(255) DEFAULT NULL,
+  `dept_url` varchar(255) DEFAULT NULL,
+  `total_view` int(255) DEFAULT NULL,
+  `total_article` int(255) DEFAULT NULL,
+  `total_patient` int(255) DEFAULT NULL,
+  `wexin_baodao` int(255) DEFAULT NULL,
+  `total_baodao` int(255) DEFAULT NULL,
+  `total_vote` int(255) DEFAULT NULL,
+  `total_thank_letter` int(255) DEFAULT NULL,
+  `total_gift` int(255) DEFAULT NULL,
+  `start_time` datetime DEFAULT NULL,
+  `doc_title` varchar(255) DEFAULT NULL,
+  KEY `ix_hos_id` (`hos_id`),
+  KEY `ix_hos_name` (`hos_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for docto_tmp
+-- ----------------------------
+DROP TABLE IF EXISTS `docto_tmp`;
+CREATE TABLE `docto_tmp` (
+  `doc_id` varchar(45) NOT NULL,
   `doc_name` varchar(45) DEFAULT NULL,
   `home_url` varchar(255) DEFAULT NULL,
   `info_url` varchar(255) DEFAULT NULL,
@@ -100,6 +131,7 @@ CREATE TABLE `duihua` (
   `doc_id` varchar(145) DEFAULT NULL,
   `pat_id` varchar(145) DEFAULT NULL,
   `content` text,
+  `dui_type` varchar(255) DEFAULT NULL COMMENT '文本,\r\n报到',
   PRIMARY KEY (`dui_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
 
@@ -152,6 +184,34 @@ CREATE TABLE `wenda` (
   `hos_dept` varchar(255) DEFAULT NULL,
   `drug` text,
   `zhiliao` text,
+  `con_cat_name2` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`wen_id`),
   KEY `ix_doc_id` (`doc_id`) USING HASH
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for zixun
+-- ----------------------------
+DROP TABLE IF EXISTS `zixun`;
+CREATE TABLE `zixun` (
+  `zixun_id` varchar(255) NOT NULL,
+  `doc_id` varchar(255) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `pat_id` varchar(255) DEFAULT NULL,
+  `post_time` datetime DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `jibing` varchar(255) DEFAULT NULL,
+  `total_chat_num` int(11) DEFAULT NULL,
+  `doc_chat_num` int(11) DEFAULT NULL,
+  `pat_chat_num` int(11) DEFAULT NULL,
+  `gift_flag` int(1) DEFAULT '0',
+  `hot_gift_flag` int(1) DEFAULT '0',
+  `pay_flag` int(1) DEFAULT '0',
+  `ext1` varchar(255) DEFAULT NULL,
+  `ext2` varchar(255) DEFAULT NULL,
+  `ext3` varchar(255) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `status` int(1) DEFAULT NULL,
+  `tell_flag` int(1) DEFAULT '0',
+  PRIMARY KEY (`zixun_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
